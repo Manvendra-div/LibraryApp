2f89144c8f39a03100734a23a1ce9eed80a47ad5
Manvendra Singh <ms1577239@gmail.com>
2024-06-04 18:03:39 +0530

    logs removed

--- src/services/storeLS.js
+++ src/services/storeLS.js
@@ -10,14 +10,12 @@ export const storeToLocalStorage = (object) => {
   export const checkTheBook = (object) => {
     const prevArr = JSON.parse(localStorage.getItem("books")) || [];
     const exists = prevArr.some(book => JSON.stringify(book) === JSON.stringify(object));
-    console.log(exists);
     return exists;
   };
   
   export const removeFromBooks = (object) => {
     const prevArr = JSON.parse(localStorage.getItem("books")) || [];
     const value = prevArr.filter(book => JSON.stringify(book) !== JSON.stringify(object));
-    console.log(value);
     localStorage.setItem("books", JSON.stringify(value));
   };
   export const getFromLocalStorage = () => {
